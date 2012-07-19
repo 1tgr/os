50b7e3effd6d16f0f3a284b689099d4533b84cd7
Tim Robinson <tim.g.robinson@gmail.com>
2012-07-19 22:41:53 +0100

    Save and restore register context for a thread

--- Makefile
+++ Makefile
@@ -1,13 +1,13 @@
 all: kernel.bin
 
 clean:
-	rm loader.o kernel.o kernel.bin
+	rm loader.o isr.o kernel.o kernel.bin
 
-loader.o: loader.s
-	i386-elf-as -o loader.o loader.s
+.S.o:
+	i386-elf-gcc-4.3.2 -o $@ -c $<
 
-kernel.o: kernel.c
-	i386-elf-gcc-4.3.2 -Wall -Wextra -Werror -fno-builtin -nostartfiles -nodefaultlibs -std=c99 -I newlib/i386-elf/include -o kernel.o -c kernel.c 
+.c.o:
+	i386-elf-gcc-4.3.2 -Wall -Wextra -Werror -fno-builtin -nostartfiles -nodefaultlibs -std=c99 -I newlib/i386-elf/include -o $@ -c $<
 
-kernel.bin: loader.o kernel.o linker.ld
-	i386-elf-ld -L newlib/i386-elf/lib -T linker.ld -o kernel.bin loader.o kernel.o -lc -lnosys
+kernel.bin: loader.o isr.o kernel.o linker.ld
+	i386-elf-ld -L newlib/i386-elf/lib -T linker.ld -o $@ $^ -lc -lnosys
--- kernel.c
+++ kernel.c
@@ -1,10 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static unsigned char * const video = (unsigned char *)0xb8000;
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+
+typedef struct {
+	uint32_t edi, esi, ebp, esp, ebx, edx, ecx, eax;
+} i386_pusha_t;
+
+typedef struct {
+	uint32_t fault_addr;
+	i386_pusha_t regs;
+	uint32_t gs, fs, es, ds;
+	uint32_t intr, error;
+	uint32_t eip, cs, eflags;
+    union {
+        struct {
+            uint32_t esp, ss;
+        } user_mode;
+        struct {
+            uint32_t caller, arg;
+        } kernel_mode;
+    } u;
+} i386_context_t;
+
+typedef struct thread_t {
+    struct thread_t *prev, *next;
+    void *kernel_esp;
+} thread_t;
+
+void isr_switch_ret(i386_context_t *context);
+
+static uint8_t* const video = (uint8_t*)0xb8000;
 static int x, y;
+static thread_t *thread_first, *thread_last;
  
-static void outb(unsigned short port, unsigned char val ) {
+static void outb(uint16_t port, uint8_t val) {
     __asm("outb %0, %1" : : "a"(val), "Nd"(port));
 }
 
@@ -15,9 +47,14 @@ static void update_cursor(int position) {
     outb(0x3D5, position >> 8);
 }
 
+void i386_isr(void *kernel_esp, i386_context_t context) {
+    printf("returning to cs:eip = %04x:%08x, esp = %p\n", context.cs, context.eip, kernel_esp);
+    isr_switch_ret(kernel_esp);
+}
+
 int write(int file, char *ptr, int len) {
     if (file == 1) {
-        unsigned char *write = video + (y * 80 + x) * 2;
+        uint8_t *write = video + (y * 80 + x) * 2;
 
         for (int i = 0; i < len; i++) {
             switch (ptr[i]) {
@@ -46,6 +83,57 @@ int write(int file, char *ptr, int len) {
         return -1;
 }
 
+static void halt() {
+    __asm("hlt");
+}
+
+thread_t *thread_start(void (*entry)(void*), void *arg) {
+    int stack_size = 65536;
+    uint8_t *stack_start = malloc(stack_size);
+    void *stack_end = stack_start + stack_size - sizeof(i386_context_t);
+    uint16_t code_segment, data_segment;
+    __asm("mov %%cs, %0" : "=r"(code_segment));
+    __asm("mov %%ds, %0" : "=r"(data_segment));
+
+    i386_context_t context = {
+        .fault_addr = 0,
+        .regs = { 0, 0, 0, 0, 0, 0, 0, 0, },
+        .gs = data_segment,
+        .fs = data_segment,
+        .es = data_segment,
+        .ds = data_segment,
+        .intr = 0,
+        .error = 0,
+        .eip = (uint32_t) entry,
+        .cs = code_segment,
+        .eflags = 0,
+        .u.kernel_mode = {
+            .caller = (uint32_t) halt,
+            .arg = (uint32_t) arg
+        },
+    };
+    
+    *(i386_context_t *) stack_end = context;
+
+    thread_t *t = malloc(sizeof(*t));
+    t->prev = thread_last;
+    t->next = NULL;
+    t->kernel_esp = stack_end;
+
+    if (thread_last != NULL)
+        thread_last->next = t;
+
+    if (thread_first == NULL)
+        thread_first = t;
+
+    thread_last = t;
+    return t;
+}
+
+static void thread(void *arg) {
+    printf("hello from %s (%p) (%p)\n", (char *) arg, arg, &arg);
+}
+
 void kmain(void) {
     for (int i = 0; i < 80 * 25; i++) {
         video[i * 2] = ' ';
@@ -55,4 +143,7 @@ void kmain(void) {
     x = y = 0;
     update_cursor(0);
     printf("hello world\n");
+
+    thread_t *t = thread_start(thread, "thread 1");
+    isr_switch_ret(t->kernel_esp);
 }
