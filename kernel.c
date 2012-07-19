6e23c7d35a9a03ed821d342b360305ca5b98f552
Tim Robinson <tim.g.robinson@gmail.com>
2012-07-19 21:33:31 +0100

    Clear screen and update cursor position

--- kernel.c
+++ kernel.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 static unsigned char * const video = (unsigned char *)0xb8000;
 static int x, y;
  
+static void outb(unsigned short port, unsigned char val ) {
+    __asm("outb %0, %1" : : "a"(val), "Nd"(port));
+}
+
+static void update_cursor(int position) {
+    outb(0x3D4, 0x0F);
+    outb(0x3D5, position);
+    outb(0x3D4, 0x0E);
+    outb(0x3D5, position >> 8);
+}
+
 int write(int file, char *ptr, int len) {
     if (file == 1) {
         unsigned char *write = video + (y * 80 + x) * 2;
@@ -28,11 +40,19 @@ int write(int file, char *ptr, int len) {
             }
         }
 
+        update_cursor((write - video) / 2);
         return len;
     } else
         return -1;
 }
 
 void kmain(void) {
+    for (int i = 0; i < 80 * 25; i++) {
+        video[i * 2] = ' ';
+        video[i * 2 + 1] = 7;
+    }
+
+    x = y = 0;
+    update_cursor(0);
     printf("hello world\n");
 }
