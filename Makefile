9999f30054afa70c2598b4d8a323983d43a8ba39
Tim Robinson <tim.g.robinson@gmail.com>
2012-07-19 21:16:36 +0100

    printf

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ loader.o: loader.s
 	i386-elf-as -o loader.o loader.s
 
 kernel.o: kernel.c
-	i386-elf-gcc-4.3.2 -Wall -Wextra -Werror -fno-builtin -nostartfiles -nodefaultlibs -I newlib/i386-elf/include -o kernel.o -c kernel.c 
+	i386-elf-gcc-4.3.2 -Wall -Wextra -Werror -fno-builtin -nostartfiles -nodefaultlibs -std=c99 -I newlib/i386-elf/include -o kernel.o -c kernel.c 
 
 kernel.bin: loader.o kernel.o linker.ld
-	i386-elf-ld -L newlib/i386-elf/lib -T linker.ld -o kernel.bin loader.o kernel.o -lc
+	i386-elf-ld -L newlib/i386-elf/lib -T linker.ld -o kernel.bin loader.o kernel.o -lc -lnosys
--- kernel.c
+++ kernel.c
@@ -1,6 +1,38 @@
 #include <stdio.h>
+
+static unsigned char * const video = (unsigned char *)0xb8000;
+static int x, y;
  
-void kmain(void)
-{
+int write(int file, char *ptr, int len) {
+    if (file == 1) {
+        unsigned char *write = video + (y * 80 + x) * 2;
+
+        for (int i = 0; i < len; i++) {
+            switch (ptr[i]) {
+                case '\n':
+                    x = 0;
+                    y++;
+                    write = video + (y * 80 + x) * 2;
+                    break;
+
+                default:
+                    write[0] = ptr[i];
+                    write[1] = 7;
+                    write += 2;
+                    x++;
+                    if (x >= 80) {
+                        x = 0;
+                        y++;
+                    }
+                    break;
+            }
+        }
+
+        return len;
+    } else
+        return -1;
+}
+
+void kmain(void) {
     printf("hello world\n");
 }
