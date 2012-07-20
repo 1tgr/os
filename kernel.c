3f52e20c59620feb49dbb933c5b487df1029d140
Tim Robinson <tim.g.robinson@gmail.com>
2012-07-20 10:52:00 +0100

    Two threads

--- kernel.c
+++ kernel.c
@@ -13,7 +13,7 @@ typedef struct {
 	uint32_t fault_addr;
 	i386_pusha_t regs;
 	uint32_t gs, fs, es, ds;
-	uint32_t intr, error;
+	uint32_t interrupt, error;
 	uint32_t eip, cs, eflags;
     union {
         struct {
@@ -25,16 +25,29 @@ typedef struct {
     } u;
 } i386_context_t;
 
+typedef struct {
+	uint16_t limit;
+	uint16_t base_l;
+	uint8_t base_m;
+	uint8_t access;
+	uint8_t attribs;
+	uint8_t base_h;
+} __attribute__((packed)) descriptor_t;
+
+typedef struct {
+	uint16_t offset_l, selector;
+	uint8_t param_cnt, access;
+	uint16_t offset_h;
+} __attribute__((packed)) descriptor_int_t;
+
 typedef struct thread_t {
     struct thread_t *prev, *next;
     void *kernel_esp;
 } thread_t;
 
-void isr_switch_ret(i386_context_t *context);
-
 static uint8_t* const video = (uint8_t*)0xb8000;
 static int x, y;
-static thread_t *thread_first, *thread_last;
+thread_t *thread_first, *thread_last, *thread_current;
  
 static void outb(uint16_t port, uint8_t val) {
     __asm("outb %0, %1" : : "a"(val), "Nd"(port));
@@ -47,9 +60,20 @@ static void update_cursor(int position) {
     outb(0x3D5, position >> 8);
 }
 
-void i386_isr(void *kernel_esp, i386_context_t context) {
-    printf("returning to cs:eip = %04x:%08x, esp = %p\n", context.cs, context.eip, kernel_esp);
-    isr_switch_ret(kernel_esp);
+void *i386_isr(void *kernel_esp, i386_context_t context) {
+    context = context;
+
+    if (thread_current != NULL)
+        thread_current->kernel_esp = kernel_esp;
+
+    if (thread_current != NULL && thread_current->next != NULL)
+        thread_current = thread_current->next;
+    else
+        thread_current = thread_first;
+
+    outb(0x20,0x20);
+    outb(0xa0,0x20);
+    return thread_current->kernel_esp;
 }
 
 int write(int file, char *ptr, int len) {
@@ -61,6 +85,9 @@ int write(int file, char *ptr, int len) {
                 case '\n':
                     x = 0;
                     y++;
+                    if (y >= 25) {
+                        y = 0;
+                    }
                     write = video + (y * 80 + x) * 2;
                     break;
 
@@ -72,6 +99,9 @@ int write(int file, char *ptr, int len) {
                     if (x >= 80) {
                         x = 0;
                         y++;
+                        if (y >= 25) {
+                            y = 0;
+                        }
                     }
                     break;
             }
@@ -84,7 +114,9 @@ int write(int file, char *ptr, int len) {
 }
 
 static void halt() {
-    __asm("hlt");
+    while (1) {
+        __asm("hlt");
+    }
 }
 
 thread_t *thread_start(void (*entry)(void*), void *arg) {
@@ -102,11 +134,11 @@ thread_t *thread_start(void (*entry)(void*), void *arg) {
         .fs = data_segment,
         .es = data_segment,
         .ds = data_segment,
-        .intr = 0,
+        .interrupt = 0,
         .error = 0,
         .eip = (uint32_t) entry,
         .cs = code_segment,
-        .eflags = 0,
+        .eflags = 0x202,
         .u.kernel_mode = {
             .caller = (uint32_t) halt,
             .arg = (uint32_t) arg
@@ -130,8 +162,44 @@ thread_t *thread_start(void (*entry)(void*), void *arg) {
     return t;
 }
 
-static void thread(void *arg) {
+static void thread1_entry(void *arg) {
     printf("hello from %s (%p) (%p)\n", (char *) arg, arg, &arg);
+    while (1) {
+        printf("1");
+        fflush(stdout);
+        __asm("hlt");
+    }
+}
+
+static void thread2_entry(void *arg) {
+    printf("hello from %s (%p) (%p)\n", (char *) arg, arg, &arg);
+    while (1) {
+        printf("2");
+        fflush(stdout);
+        __asm("hlt");
+    }
+}
+
+#define exception(n) void exception_##n();
+#define irq(n) void irq_##n();
+#define interrupt(n) void interrupt_##n();
+#include "interrupt_handlers.h"
+#undef exception
+#undef irq
+#undef interrupt
+
+static void set_idt(descriptor_int_t *d, uint16_t code_segment, void (*handler)()) {
+    uint32_t offset = (uint32_t) handler;
+    d->offset_l = offset;
+    d->selector = code_segment;
+    d->param_cnt = 0;
+    d->access = 0x8e;
+    d->offset_h = offset >> 16;
+}
+
+static void lidt(void *base, unsigned int size) {
+    uint32_t i[] = { size << 16, (uint32_t) base };
+    __asm( "lidt (%0)" : : "p"(((char *) i)+2) );
 }
 
 void kmain(void) {
@@ -144,6 +212,25 @@ void kmain(void) {
     update_cursor(0);
     printf("hello world\n");
 
-    thread_t *t = thread_start(thread, "thread 1");
-    isr_switch_ret(t->kernel_esp);
+    descriptor_int_t *idt = malloc(sizeof(*idt) * 256);
+    uint16_t code_segment;
+    __asm("mov %%cs, %0" : "=r"(code_segment));
+
+#define exception(n) set_idt(idt + 0x##n, code_segment, exception_##n);
+#define irq(n) set_idt(idt + 0x##n, code_segment, irq_##n);
+#define interrupt(n) set_idt(idt + 0x##n, code_segment, interrupt_##n);
+#include "interrupt_handlers.h"
+#undef exception
+#undef irq
+#undef interrupt
+
+    for (int i = 0x31; i < 256; i++) {
+        set_idt(idt + i, code_segment, interrupt_30);
+    }
+
+    lidt(idt, sizeof(*idt) * 256);
+    thread_start(thread1_entry, "thread 1");
+    thread_start(thread2_entry, "thread 2");
+    __asm("sti");
+    halt();
 }
