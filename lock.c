7b23a3b264f80c9e0f57b36b1bba309dd34d7718
Tim Robinson <tim.g.robinson@gmail.com>
2012-08-20 22:25:04 +0100

    Inline asm -> GCC extensions

--- lock.c
+++ lock.c
@@ -1,13 +1,5 @@
 #include "lock.h"
 
-int exchange(int value, int *at) {   
-    int result;
-    __asm (
-        "lock\n"
-        "xchgl %0, %1" : "+m" (*at), "=g" (result) : "1" (value) : "cc");
-    return result;
-} 
-
 void lock_enter(lock_t *l) {
     uint32_t flags;
     __asm(
@@ -15,7 +7,7 @@ void lock_enter(lock_t *l) {
         "popl %0\n"
         "cli" : "=g" (flags));
 
-    while (exchange(1, &l->count))
+    while (__sync_lock_test_and_set(&l->count, 1))
         ;
 
     l->flags = flags;
@@ -23,7 +15,7 @@ void lock_enter(lock_t *l) {
 
 void lock_leave(lock_t *l) {
     uint32_t flags = l->flags;
-    l->count = 0;
+    __sync_lock_release(&l->count);
     __asm(
         "pushl %0\n"
         "popf" : : "g" (flags));
